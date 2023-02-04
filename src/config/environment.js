027af902810e82996c9e3ce00767e95608cb30ae
MubashirLabar <61319180+MubashirLabar@users.noreply.github.com>
2023-02-04 23:13:25 +0500

    made changes into config file

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,7 +1,7 @@
 const url = {
   baseUrl:
     process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
-      ? "http://localhost:5000/"
+      ? "http://localhost:5001/"
       : "https://real-estate-be.vercel.app/",
 };
 export { url };
